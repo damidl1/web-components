31448dd6acad990f5885a39595fb32eb2cc48787
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-08-02 12:23:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,25 @@ function changeTitleColor() {
     header.setAttribute('title-color', 'pink'); // va a cambiare attributo all'header
     
 }
+
+
+
+
+
+// esempio fetch per task reddit
+
+fetch('https://www.reddit.com/r/aww/new.json')
+.then(resp => resp.json())
+.then(res => {
+    const data = res.data;
+    const posts = data.children;
+    console.log(posts);
+});
+
+//creare una pagina web che permetta di visualizzare 
+//gli ultimi 25 post del subreddit /aww ('https://www.reddit.com/r/aww/new.json')
+//usare i web component
+
+//1) permettere all'utente di cambiare il tema
+
+
